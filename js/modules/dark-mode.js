32e4154ba261c9873a68dbf9d81adc5fbede8c83
LipzDev <filipe.gt1998@outlook.com>
2021-02-02 18:45:45 -0300

    Arquivos

--- js/modules/dark-mode.js
+++ js/modules/dark-mode.js
@@ -7,13 +7,13 @@ export default function initDarkMode(){
       buttonSelect.classList.toggle('button-select-active');
       
       if(buttonSelect.classList.contains('button-select-active')){
-            textMode.innerHTML="Light Mode";      
+            textMode.innerHTML="Light Theme";      
          
-            colors.style="--cor-1: #2e2e2e; --cor-2: #3b3b3b; --cor-3: rgb(36, 36, 36); --cor-4: rgba(0, 0, 0, 0.568); --cor-5: rgb(51, 51, 51); --cor-6: rgb(184, 132, 253); --cor-7: rgb(212, 212, 212);";
+            colors.style="--cor-1: #2e2e2e; --cor-2: #3b3b3b; --cor-3: rgba(115, 115, 115, 0.110); --cor-4: rgba(0, 0, 0, 0.568); --cor-5: rgb(0, 0, 0, 0.150); --cor-6: rgb(184, 132, 253); --cor-7: rgb(212, 212, 212);";
            
          
          } else {
-            textMode.innerHTML="Dark Mode";  
+            textMode.innerHTML="Dark Theme";  
 
             colors.style="--cor-1: #ecf0f5; --cor-2: #e1e9f1; --cor-3: #edf1f5; --cor-4: rgba(163, 163, 163, 0.863); --cor-5: rgb(224, 224, 224); --cor-6: rgb(39, 52, 94); --cor-7: rgb(56, 56, 56);";  
          }    
