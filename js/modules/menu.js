93712d5e44b7cc3425dea477ffafe3ea618551ac
LipzDev <filipe.gt1998@outlook.com>
2021-02-02 18:06:11 -0300

    Arquivos

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -1,12 +1,18 @@
 export default function initMenu(){
-   let menu = document.querySelectorAll('.menu-item');
-   
-   menu.forEach((item) =>{
+   let menu = document.querySelectorAll('.menu-item');   
 
-      item.addEventListener('click', function(){   
-         item.classList.toggle('menu-item-actived');          
+   menu.forEach((item) => {
+      item.addEventListener('click', function(){             
+         
+         let buttonActived = document.querySelector('.menu-item-actived');
+
+         if(buttonActived.classList.contains('menu-item-actived')){
+             buttonActived.classList.remove('menu-item-actived');
+         }
+
+         item.classList.add('menu-item-actived'); 
+
       });
-
    });
 
 }
